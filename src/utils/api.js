4bef14188e10c87378408fcbf4cc3c3679033229
Pradeepa-6291 <pradeepa.c2024aids@sece.ac.in>
2025-12-18 10:06:13 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = "https://grocify-1-ricx.onrender.com:5000";
 
 // Test backend connectivity
 const testBackend = async () => {
